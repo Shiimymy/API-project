b6ba211b90b1e5fa8894bca53239f0d0d7d8c3ba
Shiimymy <>
2023-10-09 19:22:29 +0000

    Add other functions to display in modale API answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,38 @@ const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
+document.getElementById("submit").addEventListener("click", e => postForm(e));
+
+async function postForm(e) {
+    const form = new FormData(document.getElementById("checksform"));
+
+    /*The variable "e" in the loop is not the same "e" event 
+    object that we pass into the function. To test if API is working : 
+    for (let e of form.entries()) {
+       console.log(e);
+    }
+    */
+
+    const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Authorization": API_KEY,
+        },
+        /* to send form to the API add into the request, 
+        to attached it as body of the request: */
+        body: form,
+    });
+
+    // convert the response to json and display it : 
+    const data = await response.json();
+
+    if (response.ok) {
+        displayErrors(data);
+    } else {
+        throw new Error(data.error);
+    }
+
+}
 
 async function getStatus(e) {
 
@@ -13,10 +45,33 @@ async function getStatus(e) {
     const data = await response.json();
 
     if (response.ok) {
-        displayStatus(data.expiry);
+        displayStatus(data);
     } else {
         throw new Error(data.error);
     }
+
+}
+
+function displayErrors(data) {
+
+    let results = "";
+
+    let heading = `JSHint Results for ${data.file}`;
+    if (data.total_errors === 0) {
+        results = `<div class="no_errors">No errors reported!</div>`;
+    } else {
+        results = `<div>Total Errors: <span class="error_count">${data.total_errors}</span></div>`;
+        for (let error of data.error_list) {
+            results += `<div>At line <span class="line">${error.line}</span>, `;
+            results += `column <span class="column">${error.col}:</span></div>`;
+            results += `<div class="error">${error.error}</div>`;
+        }
+    }
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+    // display the modal : 
+    resultsModal.show();
 }
 
 function displayStatus(data) {
@@ -28,4 +83,6 @@ function displayStatus(data) {
     document.getElementById("resultsModalTitle").innerText = heading;
     document.getElementById("results-content").innerHTML = results;
     resultsModal.show();
+
 }
+
